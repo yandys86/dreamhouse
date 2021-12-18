7675918f42d3a43ebbfccab84f51247b21ba633a
yandys <yandys.alfonso86@gmail.com>
2021-12-17 23:14:33 -0600

    update

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import Home from './containers/Home';
-import Saleshouselist from './containers/Saleshouselist';
-import Renthouselist from './containers/Renthouselist';
+
 import Sellhouse from './containers/Sellhouse';
 import Search from './containers/Search';
 import Savehouse from './containers/Savehouse';
@@ -10,21 +9,35 @@ import ViewHome from './containers/ViewHome';
 import SignUp from './containers/SignUp';
 import SignIn from './containers/SignIn';
 import Thankyou from './containers/Thankyou';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser } from './reducks/users/selectors';
+import { fetchUserFromLocalStorage } from './reducks/users/operations';
 
 const Router = () => {
+    const dispatch = useDispatch();
+    const selector = useSelector(state => state);
+    const user = getUser(selector);
+    const token = user ? user.token : null;
+
+    console.log('Token', token);
+
+    useEffect(() => {
+        dispatch(fetchUserFromLocalStorage());
+        // eslint-disable-next-line
+    }, []);
     return (
         <>
             <Switch>
                 <Route exact path={'/'} component={Home} />
-                <Route exact path={'/Saleshouselist'} component={Saleshouselist} />
-                <Route exact path={'/Sellhouse'} component={Sellhouse} />
-                <Route exact path={'/Renthouselist'} component={Renthouselist} />
+
+                <Route exact path={'/Sellhouse'} component={token ? Sellhouse : SignIn} />
+
                 <Route exact path={'/Savehouse'} component={Savehouse} />
                 <Route exact path={'/ViewHome'} component={ViewHome} />
                 <Route exact path={'/Signup'} component={SignUp} />
                 <Route exact path={'/Signin'} component={SignIn} />
                 <Route exact path={'/Thankyou'} component={Thankyou} />
-                <Route exact path={'/Search'} component={Search} />
+                <Route exact path={'/Search'} component={token ? Search : SignIn} />
             </Switch>
         </>
     );
